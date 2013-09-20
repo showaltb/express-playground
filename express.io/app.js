2be51d3e6352c96c49022e0e35660c5f040b1309
Bob Showalter <showaltb@gmail.com>
2013-09-20 11:49:50 -0400

    Send multiple events with timeout

--- express.io/app.js
+++ express.io/app.js
@@ -1,20 +1,40 @@
-express = require('express.io');
-app = express();
-app.http().io();
+(function () {
 
-// serve static assets from public
-app.use(express.static('public'));
+  var express, app;
 
-// Setup the ready route, and emit talk event.
-app.io.route('ready', function(req) {
-    req.io.emit('talk', {
-        message: 'io event from an io route on the server'
-    });
+  var talker = function(req) {
+    var n = 0;
+    var say = function() {
+      n++;
+      var now = new Date().getTime();
+      req.io.emit('talk', { message: "This is event " + n + " at " + now });
+      setTimeout(say, 2000);
+    };
+    say();
+  };
+
+  express = require('express.io');
+  app = express();
+  app.http().io();
+
+  // serve static assets from public
+  app.use(express.static('public'));
+
+  // Setup the ready route, and emit talk event.
+  app.io.route('ready', function(req) {
+      talker(req);
+      /*
+      req.io.emit('talk', {
+message: 'io event from an io route on the server'
 });
+      */
+      });
 
-// Send the client html.
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/client.html');
-});
+  // Send the client html.
+  app.get('/', function(req, res) {
+      res.sendfile(__dirname + '/client.html');
+      });
 
-app.listen(3000);
+  app.listen(3000);
+
+})();
