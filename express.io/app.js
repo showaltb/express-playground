e658a8232747c63dded938c74e26a23a33d923d6
Bob Showalter <showaltb@gmail.com>
2013-09-20 11:22:13 -0400

    Show messages in page

--- express.io/app.js
+++ express.io/app.js
@@ -1,16 +1,20 @@
-app = require('express.io')()
-app.http().io()
+express = require('express.io');
+app = express();
+app.http().io();
+
+// serve static assets from public
+app.use(express.static('public'));
 
 // Setup the ready route, and emit talk event.
 app.io.route('ready', function(req) {
     req.io.emit('talk', {
         message: 'io event from an io route on the server'
-    })
-})
+    });
+});
 
 // Send the client html.
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/client.html')
-})
+    res.sendfile(__dirname + '/client.html');
+});
 
-app.listen(3000)
+app.listen(3000);
